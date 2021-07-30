4ddf72f05a6bef4476c25ad47ead5590b3fedb4b
jeun-990806 <kjeun990806@gmail.com>
2021-07-30 18:22:03 +0900

    0730

--- main.py
+++ main.py
@@ -7,21 +7,3 @@ import nltk
 urls = fm.openData('crawled/section0-searchResult.list')
 result, = ds.getSymbolicConstants(urls)
 pprint.pprint(result)
-
-
-'''
-libFunctionData = fm.open_data('glibc.dict')
-useFlags = {}
-useModes = {}
-useTypes = {}
-
-for name in libFunctionData.keys():
-    for i in range(0, len(libFunctionData[name])):
-        for j in range(0, len(libFunctionData[name][i]['arguments'])):
-            if 'flag' in libFunctionData[name][i]['arguments'][j]:
-                useFlags[name] = libFunctionData[name][i]['url']
-            if 'mode' in libFunctionData[name][i]['arguments'][j]:
-                useModes[name] = libFunctionData[name][i]['url']
-            if 'type' in libFunctionData[name][i]['arguments'][j]:
-                useTypes[name] = libFunctionData[name][i]['url']
-'''
