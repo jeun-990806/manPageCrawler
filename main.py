26efec1f4feb9d314fdfdb5b3d3b2949ec2aabd9
jeun-990806 <kjeun990806@gmail.com>
2021-07-30 18:22:24 +0900

    0730

--- main.py
+++ main.py
@@ -3,7 +3,3 @@ import pprint
 import fileManagement as fm
 import documentScanner as ds
 import nltk
-
-urls = fm.openData('crawled/section0-searchResult.list')
-result, = ds.getSymbolicConstants(urls)
-pprint.pprint(result)
